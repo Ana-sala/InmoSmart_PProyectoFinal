f771efd799aef48105364c0a0e26a2273b002c29
Ana-sala <anamariasm0510@gmail.com>
2026-05-23 16:08:32 -0500

    Add MainApp and all FXML views - Ana Maria

--- InmoSmartPP_Final/src/main/java/co/edu/uniquindio/poo/inmosmartpp_final/MainApp.java
+++ InmoSmartPP_Final/src/main/java/co/edu/uniquindio/poo/inmosmartpp_final/MainApp.java
@@ -1,45 +1,68 @@
 package co.edu.uniquindio.poo.inmosmartpp_final;
 
+import co.edu.uniquindio.poo.inmosmartpp_final.model.*;
+import co.edu.uniquindio.poo.inmosmartpp_final.viewController.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 public class MainApp extends Application {
-
     private Stage primaryStage;
-    public static InmoSmartApp inmoSmartApp;
+    public static InmoSmart inmoSmart = new InmoSmart("InmoSmart");
 
     @Override
-    public void start(Stage primaryStage) throws IOException {
+    public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("InmoSmart - Plataforma Inmobiliaria");
-        inmoSmartApp = new InmoSmartApp("InmoSmart");
-        abrirVistaPrincipal();
+        inicializarDatos();
+        openPrimary();
     }
 
-    private void abrirVistaPrincipal() {
+    private void inicializarDatos() {
+        Vendedor v1 = new Vendedor("V1", "Carlos López", "100200300", "3101234567", "carlos@mail.com");
+        Comprador c1 = new Comprador("C1", "María García", "200300400", "3209876543", "maria@mail.com");
+        inmoSmart.registrarUsuario(v1);
+        inmoSmart.registrarUsuario(c1);
+        Inmueble i1 = new Inmueble("INM001", TipoInmueble.APARTAMENTO, "Cra 15 #23-45", "Armenia", 80, 250000000, v1);
+        inmoSmart.publicarInmueble(i1, "Apartamento moderno en el centro");
+        Inmueble i2 = new Inmueble("INM002", TipoInmueble.CASA, "Cll 8 #12-30", "Calarcá", 120, 180000000, v1);
+        inmoSmart.publicarInmueble(i2, "Casa familiar amplia");
+    }
+
+    public void openPrimary() {
         try {
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(
-                    MainApp.class.getResource("primary-view.fxml")
-            );
-            VBox rootLayout = loader.load();
-            Scene scene = new Scene(rootLayout);
-            primaryStage.setScene(scene);
+            loader.setLocation(MainApp.class.getResource("primary.fxml"));
+            VBox root = loader.load();
+            PrimaryViewController ctrl = loader.getController();
+            ctrl.setApp(this);
+            primaryStage.setScene(new Scene(root));
             primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        } catch (IOException e) { e.printStackTrace(); }
     }
 
-    public Stage getPrimaryStage() {
-        return primaryStage;
+    public void openUsuarios() { cargarVista("usuarios.fxml", UsuarioViewController.class); }
+    public void openInmuebles() { cargarVista("inmuebles.fxml", InmuebleViewController.class); }
+    public void openOfertas() { cargarVista("ofertas.fxml", OfertaViewController.class); }
+    public void openTransacciones() { cargarVista("transacciones.fxml", TransaccionViewController.class); }
+    public void openReportes() { cargarVista("reportes.fxml", ReporteViewController.class); }
+
+    @SuppressWarnings("unchecked")
+    private <T> void cargarVista(String fxml, Class<T> controllerClass) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource(fxml));
+            AnchorPane root = loader.load();
+            Object ctrl = loader.getController();
+            controllerClass.getMethod("setApp", MainApp.class).invoke(ctrl, this);
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        } catch (Exception e) { e.printStackTrace(); }
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
+    public static void main(String[] args) { launch(); }
 }
